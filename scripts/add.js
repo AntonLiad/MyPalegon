97792cae66c40d60df0e0d33627799925d9d1a87
Anton <antonlaid@yandex.ru>
2022-11-26 14:32:45 +0300

    Добавление попапа и немного подправили вид самого сайта

--- scripts/add.js
+++ scripts/add.js
@@ -1,6 +1,16 @@
 const buttonOpenPopupUser = document.querySelector('.menu-popup__name');
 const popupUser = document.querySelector('.popuo_user');
 const popupUserClose = popupUser.querySelector('.popup__button-close');
+const buttonSevAbout = document.querySelector('.popup__button');
+const aboutTitle = document.querySelector('.about__title');
+
+// передача от попап к форме 
+
+const titleName = document.querySelector('.about__title');
+const titleJob = document.querySelector('.about__job');
+
+const popupInputName = document.querySelector('.popup__input-name');
+const popupInputJob = document.querySelector('.popup__input-job');
 
 buttonOpenPopupUser.addEventListener('click', () => {
     openPopup(popupUser)
@@ -17,3 +27,20 @@ function openPopup(window) {
 function buttonClose(window) {
     window.classList.remove('popuo_user-activ');
 }
+
+document.addEventListener('keydown', closePopupEsc);
+
+function closePopupEsc (evt) {
+    if (evt.key === 'Escape') {
+        buttonClose(popupUser)
+    }
+}
+
+buttonSevAbout.addEventListener('submit', dataTransmissionPopup)
+
+function dataTransmissionPopup(evt) {
+evt.preventDefault();
+titleName.textContent = popupInputName.value;
+titleJob.textContent = popupInputJob.value
+}
+
